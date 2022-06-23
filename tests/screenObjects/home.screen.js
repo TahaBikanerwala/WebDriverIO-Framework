098480aa480551e9bfaee30cf5f73ca492b8959e
Incubyte-Taha <incubyte-taha@pop-os.localdomain>
2022-06-23 16:38:25 +0530

    Added Footer Nav

--- tests/screenObjects/home.screen.js
+++ tests/screenObjects/home.screen.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import FooterNav from "./footer.nav"
 
@@ -6,23 +5,15 @@ const CARE_HEADING = "~Care"
 const SCHEDULE_THERAPY_BTN = "~Schedule a therapy visit"
 
 class HomeScreen extends FooterNav{
-=======
-
-const CARE_HEADING = "~Care"
-class HomeScreen{
->>>>>>> main
     get careHeading(){
         return $(CARE_HEADING)
     }
 
-<<<<<<< HEAD
     get scheduleTherapyBtn(){
         $(SCHEDULE_THERAPY_BTN).waitForDisplayed()
         return $(SCHEDULE_THERAPY_BTN)
     }
 
-=======
->>>>>>> main
 }
 
 export default new HomeScreen();
